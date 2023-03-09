f74e7713b3a4dfd562bf3876f97afea5214e7e93
Eprata21 <eprata12tesfaye@gmail.com>
2023-03-09 15:22:29 -0800

    Build my own calculator!

--- 0x02-python-import_modules/102-magic_calculation.py
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python3
+
+
 def magic_calculation(a, b):
-    from  magic_calculation import add, sub
+    from magic_calculation_102 import add, sub
     if a < b:
         c = add(a, b)
-        for x in range(4, 6):
-            c = add(a, x)
-            return (c)
+        for i in range(4, 6):
+            c = add(c, i)
+        return (c)
     else:
-        return(sub(a, b))
+        return (sub(a, b))
