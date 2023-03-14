de108fde91f88c70842bddcc039fb40a11280428
Eprata21 <eprata12tesfaye@gmail.com>
2023-03-13 18:15:36 -0700

    Can you C me now?

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,3 +1,7 @@
 #!/usr/bin/python3
 def no_c(my_string):
-    return my_string.translate({ord(i):None for i in "cC"})
+    new_string = ""
+    for char in  my_string:
+        if char not in "cC":
+            new_string += char
+    return new_string
