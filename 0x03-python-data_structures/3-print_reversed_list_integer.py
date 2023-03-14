7fbf4cb1aef8eb98476e2de73e95c2f3f3460fbc
Eprata21 <eprata12tesfaye@gmail.com>
2023-03-13 17:42:32 -0700

    reverse order

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -2,3 +2,4 @@ def print_reversed_list_integer(my_list=[]):
     for x in range(len(my_list)-1, -1, -1):
         if isinstance(my_list[x], int):
             print("{:d}".format(my_list[x]))
+~                                             
