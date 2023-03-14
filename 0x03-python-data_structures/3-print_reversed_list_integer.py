0e59b7ba0a3f8b63309bb0acb6554a22595b79d3
Eprata21 <eprata12tesfaye@gmail.com>
2023-03-13 19:37:02 -0700

    done e=with done

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,5 +1,3 @@
 def print_reversed_list_integer(my_list=[]):
-    for x in range(len(my_list)-1, -1, -1):
-        if isinstance(my_list[x], int):
-            print("{:d}".format(my_list[x]))
-~                                             
+    for i in range(len(my_list)-1, -1, -1):
+        print("{:d}".format(my_list[i]))
--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
     for i in matrix:
-        l = 1
+        x = 1
         for j in i:
-            if l == len(i):
+            if x == len(i):
                 print("{:d}".format(j), end="")
             else:
                 print("{:d}".format(j), end=" ")
-            l = l + 1
+            x = x + 1
         print()
