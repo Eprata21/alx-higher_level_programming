f15d981cd5cada3f44b539c17f3624acbd96e119
Eprata21 <eprata12tesfaye@gmail.com>
2023-04-10 19:25:23 -0700

    python in heritance is very usefull in class

--- 0x0A-python-inheritance/11-square.py
+++ 0x0A-python-inheritance/11-square.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 Rectangle = __import__('9-rectangle').Rectangle
 """
-class of rectangle
+===================================
+module with class BaseGeometry
+===================================
 """
 
 
@@ -14,11 +16,3 @@ class Square(Rectangle):
         super().__init__(size, size)
         self.integer_validator("size", size)
         self.__size = size
-
-    def area(self):
-        """rectangle area"""
-
-        return self.__size ** 2
-
-    def __str__(self):
-        return "[Square] {}/{}".format(self.__size, self.__size)
