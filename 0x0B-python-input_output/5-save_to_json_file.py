fe0459c954637ff25e3915134e0b987a6d6ac3a4
Eprata21 <eprata12tesfaye@gmail.com>
2023-04-12 13:57:19 -0700

    save json

---
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+"""
+save to json file
+"""
+
+
+import json
+
+
+def save_to_json_file(my_obj, filename):
+    """function that writes an Object to a text file, using a JSON"""
+    with open(filename, "w") as f:
+        json.dump(my_obj, f)
