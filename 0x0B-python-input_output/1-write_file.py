1c97da53243c26a95d2f8492ba5feb0b9b608de4
Eprata21 <eprata12tesfaye@gmail.com>
2023-04-12 10:43:37 -0700

    read

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -1,4 +1,10 @@
 #!/usr/bin/python3
+
+"""
+file write input
+"""
+
+
 def write_file(filename="", text=""):
     """ function that returns the number of lines of a text file """
     with open(filename, "w", encoding="UTF-8") as f:
