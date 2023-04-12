480b40714f3a8c09dac601696b892e8a8b2c8fdb
Eprata21 <eprata12tesfaye@gmail.com>
2023-04-11 18:49:31 -0700

    write

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
-def number_of_lines(filename=""):
+def write_file(filename="", text=""):
     """ function that returns the number of lines of a text file """
 
-    with open(filename) as f:
-        count = len(f.readlines())
-    return count
+    with open('my_first_file.txt', encoding="UTF-8") as f:
+        return len(list(f))
