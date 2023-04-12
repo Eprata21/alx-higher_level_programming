b5b61f1577e56e58089bc0669e329769718e46be
Eprata21 <eprata12tesfaye@gmail.com>
2023-04-12 11:12:58 -0700

    area

--- 0x0A-python-inheritance/11-square.py
+++ 0x0A-python-inheritance/11-square.py
@@ -7,12 +7,12 @@ python inheritance
 
 Rectangle = __import__('9-rectangle').Rectangle
 
+
 class Square(Rectangle):
     """Square class that inherits from Rectangle that inherits BaseGeometry"""
 
     def __init__(self, size):
         """Method for initialized the attrubutes"""
-        super().__init__(size, size)
         self.integer_validator("size", size)
         self.__size = size
 
